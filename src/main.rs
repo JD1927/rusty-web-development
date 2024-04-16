7e10e18a0f98ede9d27b41ac82b32089cd81cfa6
Juan Aguirre <j1927aguirre@gmail.com>
2024-04-15 21:08:09 -0500

    feat: creates a new question object and fixes errors

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::{
+    io::{Error, ErrorKind},
+    str::FromStr,
+};
+
+#[derive(Debug)]
 struct Question {
     id: QuestionId,
     title: String,
     content: String,
     tags: Option<Vec<String>>,
 }
+
+#[derive(Debug)]
 struct QuestionId(String);
 
 impl Question {
@@ -17,6 +25,24 @@ impl Question {
     }
 }
 
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId::from_str("1").expect("No id provided"),
+        "First Question".to_string(),
+        "Content of the question".to_string(),
+        Some(vec!["faq".to_string()]),
+    );
+
+    println!("{:#?}", question);
 }

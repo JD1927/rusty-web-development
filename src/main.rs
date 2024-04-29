3b7313248bc16a11c4e983f22d6cde1acc90830f
Juan Aguirre <j1927aguirre@gmail.com>
2024-04-28 21:50:42 -0500

    feat: add delete question feature
    using the same approach as in the update function

--- src/main.rs
+++ src/main.rs
@@ -111,6 +111,16 @@ async fn get_questions(
     }
 }
 
+async fn delete_question(id: String, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
+    match store.questions.write().await.remove(&QuestionId(id)) {
+        Some(_) => Ok(warp::reply::with_status(
+            "Question deleted!",
+            StatusCode::OK,
+        )),
+        None => Err(warp::reject::custom(Error::QuestionNotFound)),
+    }
+}
+
 fn extract_pagination(
     params: HashMap<String, String>,
     store_length: usize,
@@ -184,9 +194,17 @@ async fn main() {
         .and(warp::body::json())
         .and_then(update_question);
 
+    let delete_question = warp::delete()
+        .and(warp::path("questions"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and_then(delete_question);
+
     let routes = get_questions
         .or(add_question)
         .or(update_question)
+        .or(delete_question)
         .with(cors)
         .recover(return_error);
 
